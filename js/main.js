f1a1b428411477811af8a1e1a0bf1eb50f07af8d
Mary <mblnkinakk@gmail.com>
2017-04-02 17:30:13 +0300

    Changes in media queries

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-﻿	        $(function() {
+﻿$(function(){
 	            $(".room").click(function() {
 	                $(this).siblings( ".hide_text" ).toggle("slow");
 	            });
-	        });
+	            /*hide/show menu*/
+	            $('.menuToggle').on('click', function(){
+					$('.menu').toggle(300, function(){
+	    				if($(this).css('display') ==='none'){
+	        				$(this).removeAttr('style');
+	        			}
+	    });
+	});
+});
+
+
